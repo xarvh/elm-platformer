2398b076d975787797f0f5886077e498b0836558
xarvh <francesco.orsenigo@gmail.com>
2019-04-27 17:25:43 +0200

    Use font-size attribute instead of style

--- src/SpeechBubble.elm
+++ src/SpeechBubble.elm
@@ -5,6 +5,7 @@ import Game exposing (..)
 import Math.Matrix4 as Mat4 exposing (Mat4)
 import Svg exposing (Svg)
 import Svg.Attributes as SA
+import Vector exposing (Vector)
 import Viewport.Combine
 
 
@@ -29,18 +30,18 @@ defaultDuration content =
 uNew : Maybe Id -> String -> UpdateFunction -> UpdateFunction
 uNew maybeParentId content onDone =
     let
-        render =
-            if maybeParentId == Nothing then
-                renderOffscreen
-            else
-                renderWithTail
+        args : RenderArgs
+        args =
+            { content = content
+            , offscreen = maybeParentId == Nothing
+            }
 
         init : EntityUpdateFunction
         init env _ game entity =
             toTriple
                 ( entity
                     |> appendRenderFunctions
-                        [ render content
+                        [ render args
                         ]
                     |> appendEntityUpdateFunctions
                         [ queryTextWidth
@@ -91,8 +92,14 @@ resetAll game =
 -- Render
 
 
-renderWithTail : String -> RenderFunction
-renderWithTail content env game entity =
+type alias RenderArgs =
+    { offscreen : Bool
+    , content : String
+    }
+
+
+render : RenderArgs -> RenderFunction
+render args env game entity =
     let
         textWidthAsRatio =
             cTextWidth.get entity
@@ -109,21 +116,52 @@ renderWithTail content env game entity =
         bubbleW =
             2 * margin + textW
 
-        p =
-            [ "M 0 0"
-            , "v 1"
-            , "h -2"
-            , "v " ++ String.fromFloat bubbleH
-            , "h " ++ String.fromFloat bubbleW
-            , "v " ++ String.fromFloat -bubbleH
-            , "L 0.5 1"
-            , "z"
-            ]
+        { path, offset, textX, textY } =
+            if args.offscreen then
+                let
+                    marginFromTop =
+                        1.0
+
+                    yOffset =
+                        env.visibleWorldSize.height / 2 - marginFromTop - bubbleH / 2
+                in
+                { path =
+                    [ "M " ++ String.fromFloat (bubbleW / -2) ++ " " ++ String.fromFloat (bubbleH / -2)
+                    , "v " ++ String.fromFloat bubbleH
+                    , "h " ++ String.fromFloat bubbleW
+                    , "v " ++ String.fromFloat -bubbleH
+                    , "z"
+                    ]
+                , offset = Mat4.translate3 game.cameraPosition.x (game.cameraPosition.y + yOffset) 0
+                , textX = String.fromFloat <| -bubbleW / 2 + 0.5
+                , textY = String.fromFloat <| bubbleH / 2 - 0.5
+                }
+            else
+                let
+                    xOffset =
+                        0.3
+
+                    yOffset =
+                        1.5
+                in
+                { path =
+                    [ "M 0 0"
+                    , "v 1"
+                    , "h -2"
+                    , "v " ++ String.fromFloat bubbleH
+                    , "h " ++ String.fromFloat bubbleW
+                    , "v " ++ String.fromFloat -bubbleH
+                    , "L 0.5 1"
+                    , "z"
+                    ]
+                , offset = Mat4.translate3 (entity.absolutePosition.x + xOffset) (entity.absolutePosition.y + yOffset) 0
+                , textX = "-1.5"
+                , textY = "-1.5"
+                }
     in
     Svg.g
         [ env.worldToCamera
-            |> Mat4.translate3 entity.absolutePosition.x entity.absolutePosition.y 0
-            |> Mat4.translate3 0.3 1.5 0
+            |> offset
             |> Viewport.Combine.transform
         , if textW == 0 then
             SA.opacity "0"
@@ -131,83 +169,24 @@ renderWithTail content env game entity =
             SA.class ""
         ]
         [ Svg.path
-            [ SA.d (String.join " " p)
+            [ SA.d (String.join " " path)
             , SA.fill "white"
             , SA.stroke "black"
             , SA.strokeWidth "0.1"
             ]
             []
         , Svg.text_
-            [ SA.transform "scale(1, -1)"
-            , SA.x "-1.5"
-            , SA.y "-1.5"
-            , SA.style "font-size: 1"
-            , SA.textLength <| String.fromFloat textW
-            , SA.class "speech-baloon"
+            [ SA.class "speech-baloon"
             , SA.id (String.fromInt entity.id)
+            , SA.transform "scale(1, -1)"
+            , SA.x textX
+            , SA.y textY
+            , SA.fontSize "1"
+            , if textW /= 0 then
+                SA.textLength <| String.fromFloat textW
+              else
+                SA.class ""
             ]
-            [ Svg.text content ]
-        ]
-        |> RenderableSvg 2
-
-
-renderOffscreen : String -> RenderFunction
-renderOffscreen content env game entity =
-    let
-        textWidthAsRatio =
-            cTextWidth.get entity
-
-        textW =
-            textWidthAsRatio * env.visibleWorldSize.width
-
-        margin =
-            0.5
-
-        bubbleH =
-            2 * margin + 0.6
-
-        bubbleW =
-            2 * margin + textW
-
-        marginFromTop =
-            1.0
-
-        yOffset =
-            env.visibleWorldSize.height / 2 - marginFromTop - bubbleH / 2
-
-        p =
-            [ "M " ++ String.fromFloat (bubbleW / -2) ++ " " ++ String.fromFloat (bubbleH / -2)
-            , "v " ++ String.fromFloat bubbleH
-            , "h " ++ String.fromFloat bubbleW
-            , "v " ++ String.fromFloat -bubbleH
-            , "z"
-            ]
-    in
-    Svg.g
-        [ env.worldToCamera
-            |> Mat4.translate3 game.cameraPosition.x (game.cameraPosition.y + yOffset) 0
-            |> Viewport.Combine.transform
-        , if textW == 0 then
-            SA.opacity "0"
-          else
-            SA.class ""
-        ]
-        [ Svg.path
-            [ SA.d (String.join " " p)
-            , SA.fill "white"
-            , SA.stroke "black"
-            , SA.strokeWidth "0.1"
-            ]
-            []
-        , Svg.text_
-            [ SA.transform "scale(1, -1)"
-            , SA.x <| String.fromFloat <| -bubbleW / 2 + 0.5
-            , SA.y <| String.fromFloat <| bubbleH / 2 - 0.5
-            , SA.style "font-size: 1"
-            , SA.textLength <| String.fromFloat textW
-            , SA.class "speech-baloon"
-            , SA.id (String.fromInt entity.id)
-            ]
-            [ Svg.text content ]
+            [ Svg.text args.content ]
         ]
         |> RenderableSvg 2
