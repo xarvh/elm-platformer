be727662e5df426f98848c04620eafbd30a003b9
xarvh <francesco.orsenigo@gmail.com>
2019-05-03 20:49:31 +0200

    wip

---
+++ TODO.md
@@ -0,0 +1,14 @@
+
+
+* allow transparency
+
+* prepare data with 2 layers
+
+* make editor able to work with multiple layers
+
+* make editor save
+
+
+
+
+
--- src/Tilesets/Tileset.elm
+++ src/Tilesets/Tileset.elm
@@ -1,8 +1,61 @@
 module Tileset.Bulkhead exposing (..)
 
+{-
+
+   * Map Texture
+     - Decides which tile type goes where, its values reference the Tileset Texture
+     - R, G are the normalized x, y references to the TileType used as foreground
+     - B, A are the normalized x, y references to the TileType used as background
+
+   * Tileset Texture
+     - Decides how a tile type is drawn.
+     - R is the AnimationMode and determines how the other three values are used
+     - G, B, A are used according to the value in R
+     - Where G, B, A contain durations, they will be measured in frames (ie, chunks of 1/60s)
+
+   * Sprites Texture: it is divided in tiles and determines the final pixel colors
+
+-}
+
+
+--
+-- Types used by the game
+--
+type alias Map =
+  { id : String
+  , width : Int
+  , height : Int
+  , blockers : Array (Maybe Blocker)
+  , poisByName : Dict String { x : Float, y : Float }
+  , tiles : Texture
+  , tileset : Texture
+  , sprites : Texture
+  }
+
+
+
+
+
+--
+-- Types used by the map editor
+--
+type alias EditorMap =
+  { id : String
+  , tileset : Tileset
+  , foreground : Dict { l : Int, b : Int } TileTypeReference
+  , background : Dict { l : Int, b : Int } TileTypeReference
+  , poisByName : Dict String { x : Float, y : Float }
+  }
+
+type alias TileTypeReference =
+    Int
+
 
 type alias Tileset =
     { fileName : String
+
+    -- this is used only by the editor, to know what to delete
+    , firstUnusedSprite : TileSpritesheetReference
     , tiles : List Tile
     , spriteWidthInTiles : Int
     , spriteHeightInTiles : Int
@@ -13,13 +66,12 @@ type alias Tile =
     { idWithinLayer : Int
     , layer : Int
     , maybeBlocker : Maybe Blocker
-    , animationDurationInFrames : Int
     , animationMode : AnimationMode
 
     -- How these ares used depends entirely on the AnimationMode
-    , animationOffset : Int
-    , spriteSheetReferenceA : Int
-    , spriteSheetReferenceB : Int
+    , animationDurationInFrames : Int
+    , spriteA : TileSpritesheetReference
+    , spriteB : TileSpritesheetReference
     }
 
 
